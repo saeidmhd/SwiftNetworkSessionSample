465b70dbd22d84b1c149192ee8d04c20e11b6cac
admin1 <saeid.mhd@gmail.com>
2017-04-15 12:30:13 +0430

    change to proceed!

--- NetworkSessionSample/ViewController.swift
+++ NetworkSessionSample/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+          makeGetCall()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +20,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+  
+    
+    func makeGetCall() {
+        let url = URL(string: "http://jsonplaceholder.typicode.com/users/4")
+        let session = URLSession.shared // or let session = URLSession(configuration: URLSessionConfiguration.default)
+        if let usableUrl = url {
+            let task = session.dataTask(with: usableUrl, completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let stringData = String(data: data, encoding: String.Encoding.utf8) {
+                        print(stringData) //JSONSerialization
+                        let json = try? JSONSerialization.jsonObject(with: data, options: [])
+                        print(json)
+                    }
+                }
+            })
+            task.resume()
+        }
+    }
 
 }
 
